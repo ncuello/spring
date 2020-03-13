11f014cf02fd709911ae7457f0a882276603d8ba
Nicolás <nicolascuello@outlook.com>
2020-03-13 11:34:58 -0300

    Creación de módulo ABM Usuarios

--- src/main/java/net/sysone/app/service/PerfilesServiceJPA.java
+++ src/main/java/net/sysone/app/service/PerfilesServiceJPA.java
@@ -1,10 +1,12 @@
 package net.sysone.app.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import net.sysone.app.model.Perfil;
 import net.sysone.app.repository.PerfilesRepository;
 
+@Service
 public class PerfilesServiceJPA implements IPerfilesService {
 	
 	@Autowired
